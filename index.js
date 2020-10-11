9c7761770040586e60887b1ae06bd463e70703d9
Carlito Algarin <carlito1113@yahoo.com>
2020-10-11 10:35:00 -0400

    moved my mysql connection from the index.js file into the connection.js file

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ console.table([
 ]);
 
 
-
-var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "root",
-    database: "employee_DB"
-  });
-
   // connect to the mysql server and sql database
   connection.connect(function(err) {
     if (err) throw err;
