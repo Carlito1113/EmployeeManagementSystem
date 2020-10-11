9778585a7c21b5942a6ead2a84507e36d9c99fbd
Carlito Algarin <carlito1113@yahoo.com>
2020-10-11 15:32:58 -0400

    added in the functions for the switch statement contained in the index.js file into the dbFunctions.js file

--- DB/dbFunctions.js
+++ DB/dbFunctions.js
@@ -17,7 +17,40 @@ class DB {
         });
     }
 
-    
-};
+    viewRoles() {
+        connection.query("SELECT * FROM role", function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+    }
+    addDepartment() {
+        inquirer.prompt({
+            type: "input",
+            name: "newDepartment",
+            message: "What department would you like to add?"
+        }).then(answer => {
+            connection.query("INSERT INTO DEPARTMENT name VALUES = ?", answer, function (err, res) {
+                if (err) throw err;
+                console.table(res);
+            })
+        });
+    }
+    addEmployee() {
+        connection.query("INSERT INTO DEPARTMENT name VALUES = ?", answer, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+    }
+
+    addRole() {
+        connection.query("SELECT * FROM department", function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+    }
+
+
+
+}
 
 module.exports = new DB();
